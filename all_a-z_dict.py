e487cb3ae316ba656b8fa16e9a678b61e691ef36
c4bbage <c4bbage@qq.com>
2020-02-14 14:22:02 +0800

    all a-z dict

---
+++ all_a-z_dict.py
@@ -0,0 +1,32 @@
+# # for i in range(48,58):
+# #     print(chr(i),i)
+# # for i in range(97,123):
+# #     print(chr(i),i)
+# l =[]
+# # for i in range(97,123):
+# #     l.append(chr(i))
+# #     while len(a)<3:
+# #         # print(a+chr(i))
+# #         l.append(a+chr(i))
+# # print(l)
+
+# for i in range (5):
+#     a=''
+#     while len(a)<=i:
+#         for i in range(97,123):
+#             a += chr(i)
+#             if len(a)<=1:
+#                 l.append(a)
+#             else:
+#                 continue
+#             print(a)
+# while True:
+#     a= ''
+#     if len(a)>3:
+#         break
+#     else:
+#         for i in range(97,123):
+#             a+=chr(i)
+#             if len(a)>3:
+#                 break
+#             print(a)
